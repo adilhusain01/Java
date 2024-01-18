c40afa42dc952069bb0291dd12d4ee1d92efb67f
adilhusain01 <husainadil202@gmail.com>
2024-01-18 20:25:44 +0530

    ...

---
+++ yo.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.Scanner;
+
+class yo {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int x = s.nextInt();
+        for (int i = 0; i < 50; i++) {
+            System.out.println(i);
+        }
+    }
+}
