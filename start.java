de3309ac1133e39b8a52b7758b0ed99eb9025121
adilhusain01 <husainadil202@gmail.com>
2024-01-20 15:13:19 +0530

    ...

---
+++ start.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+class start {
+    public static void main(String[] args) {
+        int x = 12;
+        long y = 123456789;
+        float z = 12.34f;
+        String s = "adil";
+        System.out.println(x + " " + y + " " + z + " " + s);
+    }
+}
---
+++ typecasting.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+class typecasting {
+    public static void main(String[] args) {
+        long x = 10;
+        int y = (int) x;
+
+        byte z = (byte) y;
+        System.out.println(y);
+    }
+}
