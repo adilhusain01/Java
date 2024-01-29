1424b62dbd4021c35f03ab9a68bbeea8e614b350
adilhusain01 <husainadil202@gmail.com>
2024-01-29 15:24:56 +0530

    ...

---
+++ 11Jan/SwitchCase.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class SwitchCase {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int x = in.nextInt();
+        int y = in.nextInt();
+        int sum = x+y;
+
+        switch (sum) {
+            case 10:
+                System.out.println("10");
+                break;
+            case 12:
+                System.out.println("12");
+                break;
+            default:
+                System.out.println("We dont know");
+                break;
+        }
+    }
+}
