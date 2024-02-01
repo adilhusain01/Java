9b958cd875c35d4262261f1b7e92aebb595eb277
adilhusain01 <husainadil202@gmail.com>
2024-02-01 16:46:48 +0530

    ...

--- 24Jan/duplicates.java
+++ 24Jan/duplicates.java
@@ -1,5 +1,13 @@
 public class duplicates {
     public static void main(String[] args) {
-        
+        int[] arr = {2, 5, 6, 7, 4, 6, 5, 1, 9, 1};
+
+        for(int i=0; i<arr.length; ++i){
+            for(int j=i+1; j<arr.length; ++j){
+                if(arr[i] == arr[j]){
+                    System.out.println(arr[i]);
+                }
+            }
+        }
     }
 }
