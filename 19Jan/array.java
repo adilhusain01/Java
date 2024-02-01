16e81e7e1cf59fcbec413c4c02b819acbbac89f6
adilhusain01 <husainadil202@gmail.com>
2024-02-01 16:43:22 +0530

    ...

---
+++ 19Jan/array.java
@@ -0,0 +1,12 @@
+public class array {
+    public static void main(String[] args) {
+        int i=0;
+
+        for(; i<5; ++i){
+            System.out.println(i);
+        }
+        if(i==5){
+            System.out.println("\n" + i);
+        }
+    }
+}
---
+++ 19Jan/upperCase.java
@@ -0,0 +1,10 @@
+class upperCase{
+    public static void main(String[] args){
+        String str = "hello how are you";
+
+        System.out.println(str.length());
+        System.out.println(str.toUpperCase());
+        System.out.println(str.indexOf("u"));
+        System.out.println(str.indexOf("how"));
+    }
+}
