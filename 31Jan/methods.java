e9fec872849ac42b817d1af1eb999376f3574e93
adilhusain01 <husainadil202@gmail.com>
2024-02-16 00:17:57 +0530

    Add methods.java file with Demo class and main method

---
+++ 31Jan/methods.java
@@ -0,0 +1,16 @@
+class Demo{
+    public int x;
+    public int y;
+
+    void fun(){
+        System.out.println("hurray");
+    }
+}
+
+public class methods {
+    public static void main(String[] args) {
+        Demo obj1 = new Demo();
+
+        obj1.fun();
+    }
+}
