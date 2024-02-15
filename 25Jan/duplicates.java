9871389304ec5d32b4fde812bb6fc0cb7758a6d4
adilhusain01 <husainadil202@gmail.com>
2024-02-15 10:17:33 +0530

    Add Java code to find duplicates in a matrix

---
+++ 25Jan/duplicates.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class duplicates {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int rows = in.nextInt();
+        int columns = in.nextInt();
+
+        int matrix[][] = new int[rows][columns];
+
+        int max = 0;
+        for(int i=0; i<rows; ++i){
+            for(int j=0; j<columns; ++j){
+                matrix[i][j] = in.nextInt();
+                if (matrix[i][j] > max) max = matrix[i][j];
+            }
+        }
+
+        int arr[] = new int[max+1];
+
+        for(int i=0; i<rows; ++i){
+            for(int j=0; j<columns; ++j){
+                arr[matrix[i][j]] = arr[matrix[i][j]] + 1;
+            }
+        }
+
+        System.out.println();
+
+        for(int i=0; i<arr.length; ++i){
+            if(arr[i] == 2){
+                System.out.println(i);
+            }
+        }
+
+        in.close();
+    }
+}
