b2824892ede801b2c1f948eac94058c0373d8507
adilhusain01 <husainadil202@gmail.com>
2024-02-16 00:17:45 +0530

    Add duplicatesTwo.java - a program to find duplicate elements in a matrix

--- 25Jan/duplicatesTwo.java
+++ 25Jan/duplicatesTwo.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class duplicates {
+public class duplicatesTwo {
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         int rows = in.nextInt();
