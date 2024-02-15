01d0e79fb8e8ea1898a9efbc6fc23a6082431cfa
adilhusain01 <husainadil202@gmail.com>
2024-02-16 01:27:16 +0530

    Add Static class with Demo class for testing static variables

---
+++ 1Feb/Static.java
@@ -0,0 +1,12 @@
+class Demo{
+    public int x;
+
+    //This variable will be auto created at the compilation and not needed to be created as object of class first to alot memory
+    public static int y;
+}
+
+public class Static {
+    public static void main(String[] args) {
+        
+    }
+}
