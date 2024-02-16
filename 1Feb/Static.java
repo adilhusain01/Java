13d08b9c3978abcd3a5dc75c5a754ca38f025cd7
adilhusain01 <husainadil202@gmail.com>
2024-02-16 16:46:53 +0530

    Add static variable and demonstrate its usage

--- 1Feb/Static.java
+++ 1Feb/Static.java
@@ -2,11 +2,17 @@ class Demo{
     public int x;
 
     //This variable will be auto created at the compilation and not needed to be created as object of class first to alot memory
+    //and its common to every object
     public static int y;
 }
 
 public class Static {
     public static void main(String[] args) {
-        
+        Demo.y = 100;
+
+        Demo obj1 = new Demo();
+        obj1.y = 200;
+        System.out.println(Demo.y);
+        System.out.println(obj1.y);
     }
 }
